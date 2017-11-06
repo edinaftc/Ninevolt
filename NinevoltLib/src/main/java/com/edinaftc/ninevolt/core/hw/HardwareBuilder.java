bed6c0e491d3e9dea664559099745a7cb91d6b7e
Richik SC <sc.richik@gmail.com>
2017-11-06 17:25:22 -0600

    update HardwareBuilder for automatic motor config based on movement and a little doc

--- NinevoltLib/src/main/java/com/edinaftc/ninevolt/core/hw/HardwareBuilder.java
+++ NinevoltLib/src/main/java/com/edinaftc/ninevolt/core/hw/HardwareBuilder.java
@@ -5,47 +5,47 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 /**
- * Created by VVMS FTC teams on 2/23/2017.
+ * Builder class to create hardware step-by-step.
+ * <a href="https://sourcemaking.com/design_patterns/builder">Follows the
+ * builder pattern</a> and allows you to daisy-chain methods.
  */
 
 public class HardwareBuilder {
 
   private HardwareMap hardwareMap;
   private Hardware building;
-  private boolean motorAllowed = false;
 
-  /* TODO: Frame and write HardwareBuilder class using Builder design pattern
-     Read here: https://sourcemaking.com/design_patterns/builder */
   public HardwareBuilder(HardwareMap hardwareMap) {
     this.hardwareMap = hardwareMap;
     building = new Hardware();
   }
 
+  @Deprecated
   public HardwareBuilder setMotorConfig(Hardware.MotorMode motorMode,
                                         DcMotor.Direction motorDirection) {
     building.setMotorMode(motorMode);
     building.setMotorDirection(motorDirection);
-    motorAllowed = true;
+    return this;
+  }
+
+  public HardwareBuilder setMotorDirection(DcMotor.Direction direction) {
+    building.setMotorDirection(direction);
     return this;
   }
 
   public HardwareBuilder addMotorFL(String hwMapName) throws Exception {
-    if (!motorAllowed) { throw new Exception("Must configure motors before adding any!"); }
     building.setMotorFL(hardwareMap.dcMotor.get(hwMapName));
     return this;
   }
   public HardwareBuilder addMotorFR(String hwMapName) throws Exception {
-    if (!motorAllowed) { throw new Exception("Must configure motors before adding any!"); }
     building.setMotorFR(hardwareMap.dcMotor.get(hwMapName));
     return this;
   }
   public HardwareBuilder addMotorBL(String hwMapName) throws Exception {
-    if (!motorAllowed) { throw new Exception("Must configure motors before adding any!"); }
     building.setMotorBL(hardwareMap.dcMotor.get(hwMapName));
     return this;
   }
-  public HardwareBuilder addMotorBR(String hwMapName) throws Exception{
-    if (!motorAllowed) { throw new Exception("Must configure motors before adding any!"); }
+  public HardwareBuilder addMotorBR(String hwMapName) throws Exception {
     building.setMotorBR(hardwareMap.dcMotor.get(hwMapName));
     return this;
   }
