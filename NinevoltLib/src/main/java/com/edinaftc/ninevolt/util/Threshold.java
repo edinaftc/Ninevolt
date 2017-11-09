584ff0106986a381f62ba26c8e1d8f7a42f682a1
Richik SC <sc.richik@gmail.com>
2017-11-08 19:09:59 -0600

    add functionality and more oop to Threshold

--- NinevoltLib/src/main/java/com/edinaftc/ninevolt/util/Threshold.java
+++ NinevoltLib/src/main/java/com/edinaftc/ninevolt/util/Threshold.java
@@ -3,23 +3,58 @@ package com.edinaftc.ninevolt.util;
 import com.qualcomm.robotcore.util.Range;
 
 public class Threshold {
-    public static double[] threshold(double target, double tolerance) {
-        tolerance = Range.clip(tolerance, 0, 1);
-        double range = tolerance * target;
-        return new double[] {target - range, target + range};
+  public static class Bounds {
+    private double lowerBound;
+    private double upperBound;
+
+    public Bounds(double lowerBound, double upperBound) {
+      this.lowerBound = lowerBound;
+      this.upperBound = upperBound;
     }
 
-    public static double[] threshold(int target, double tolerance) {
-        tolerance = Range.clip(tolerance, 0, 1);
-        double range = tolerance * target;
-        return new double[] {target - range, target + range};
+    public void setUpperBound(double upperBound) {
+      this.upperBound = upperBound;
     }
 
-    public static boolean withinThreshold(double value, double target, double tolerance) {
-        double[] range = threshold(target, tolerance);
-        if (value > range[0] && value < range[1]) {
-            return true;
-        }
-        return false;
+    public void setLowerBound(double lowerBound) {
+      this.lowerBound = lowerBound;
     }
+
+    public double getUpperBound() {
+      return upperBound;
+    }
+
+    public double getLowerBound() {
+      return lowerBound;
+    }
+
+    public boolean within(double value) {
+      return (value > lowerBound && value < upperBound);
+    }
+
+    public boolean within(int value) {
+      return within((double) value);
+    }
+  }
+  public static Bounds threshold(double target, double tolerance) {
+    tolerance = Range.clip(tolerance, 0, 1);
+    double range = tolerance * target;
+    return new Bounds(target - range, target + range);
+  }
+
+  public static Bounds threshold(int target, double tolerance) {
+    tolerance = Range.clip(tolerance, 0, 1);
+    double range = tolerance * target;
+    return new Bounds(target - range, target + range);
+  }
+
+  public static boolean withinThreshold(double value, double target, double tolerance) {
+    Bounds range = threshold(target, tolerance);
+    return range.within(value);
+  }
+
+  public static boolean withinDeviation(double value, double target, double deviation) {
+    if (value > target - deviation && value < target + deviation) return true;
+    else return false;
+  }
 }
