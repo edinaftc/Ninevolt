cc69da268431ffde44e7ba45861c2d11dd09e413
Richik SC <sc.richik@gmail.com>
2018-09-29 18:44:15 -0500

    Refactor HoloAutoOpMode to use Ninevolt 1.0

--- NinevoltLib/src/main/java/com/edinaftc/ninevolt/examples/HoloAutoOpMode.java
+++ NinevoltLib/src/main/java/com/edinaftc/ninevolt/examples/HoloAutoOpMode.java
@@ -1,8 +1,10 @@
 package com.edinaftc.ninevolt.examples;
 
+import com.edinaftc.ninevolt.Config;
+import com.edinaftc.ninevolt.Ninevolt;
 import com.edinaftc.ninevolt.core.hw.Hardware;
 import com.edinaftc.ninevolt.core.hw.HardwareBuilder;
-import com.edinaftc.ninevolt.core.hw.drivetrain.holonomic.Movement;
+import com.edinaftc.ninevolt.core.hw.drivetrain.HolonomicMovement;
 import com.edinaftc.ninevolt.util.ExceptionHandling;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -10,33 +12,32 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
- * A sample OpMode using a holonomic chassis using deprecated code.
+ * A sample autonomous OpMode using a holonomic chassis
  */
 
 @Autonomous(name = "Holonomic Square", group = "Ninevolt Sample Autonomous")
 @Disabled
-public class HoloAutoOpMode_Legacy extends LinearOpMode {
+public class HoloAutoOpMode extends LinearOpMode {
 
   private static final int PULSES_PER_MOTOR_REV = 28;
   private static final double DRIVE_GEAR_REDUCTION = 40.0;
   private static final double WHEEL_DIAMETER_INCHES = 4;
-  private static final double     PULSES_PER_INCH         = (PULSES_PER_MOTOR_REV *
-      DRIVE_GEAR_REDUCTION) /
+  private static final double PULSES_PER_INCH =
+      (PULSES_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
       (WHEEL_DIAMETER_INCHES * 3.1415);
 
   private Hardware hardware;
-  private Movement movement;
+  private HolonomicMovement movement;
 
   private void custom_init() throws Exception {
     HardwareBuilder hb = new HardwareBuilder(hardwareMap);
-    hb.setMotorConfig(Hardware.MotorMode.HOLONOMIC, DcMotor.Direction.FORWARD)
+    hb.setMotorDirection(DcMotor.Direction.FORWARD)
       .addMotorFL("motor_fl").addMotorFR("motor_fr")
       .addMotorBL("motor_bl").addMotorBR("motor_br");
     this.hardware = hb.build();
-    hb = null;
     hardware.init();
-    movement = new Movement(hardware, this, PULSES_PER_INCH);
-    movement.setVerbose(true);
+    movement = new HolonomicMovement(hardware, this, PULSES_PER_INCH);
+    Ninevolt.getConfig().setLoggingLevel(Config.LoggingLevel.VERBOSE);
   }
 
   @Override
@@ -45,15 +46,15 @@ public class HoloAutoOpMode_Legacy extends LinearOpMode {
       custom_init();
       waitForStart();
       if(opModeIsActive()) {
-        movement.yDrive(48);
-        movement.xDrive(-48);
-        movement.yDrive(-48);
-        movement.xDrive(48);
+        movement.yDrive(48, 0.5f);
+        movement.xDrive(-48, 0.5f);
+        movement.yDrive(-48, 0.5f);
+        movement.xDrive(48, 0.5f);
       }
     } catch (Exception ex) {
       ExceptionHandling.standardExceptionHandling(ex, this);
       if (ex instanceof InterruptedException) {
-        throw (InterruptedException)ex;
+        throw (InterruptedException) ex;
       }
     }
 
