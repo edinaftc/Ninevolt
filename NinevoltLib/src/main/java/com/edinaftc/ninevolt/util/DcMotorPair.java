15be99c29f12927ffe5498015b64b40da24ef0eb
Richik SC <sc.richik@gmail.com>
2018-10-01 16:38:55 -0500

    Write Javadoc for DcMotorPair class

--- NinevoltLib/src/main/java/com/edinaftc/ninevolt/util/DcMotorPair.java
+++ NinevoltLib/src/main/java/com/edinaftc/ninevolt/util/DcMotorPair.java
@@ -14,6 +14,12 @@ public class DcMotorPair {
   private DcMotor motor2;
   private boolean isSingleMotor = false;
 
+  /**
+   * Constructs a DcMotorPair containing and controlling at least one motor.
+   * @param motor1 The first motor to be controlled by this pair
+   * @param motor2 The second motor to be controlled by this pair
+   * @throws Exception Thrown when no motors are provided
+   */
   public DcMotorPair(DcMotor motor1, DcMotor motor2) throws Exception {
     this.motor1 = motor1;
     this.motor2 = motor2;
@@ -24,19 +30,36 @@ public class DcMotorPair {
     }
   }
 
+  /**
+   * Constructs a DcMotorPair controlling one motor.
+   * @param motor1 - The DcMotor to be controlled by this motor pair.
+   */
   public DcMotorPair(DcMotor motor1) {
     this.motor1 = motor1;
     isSingleMotor = true;
   }
 
+  /**
+   * Gets the first DcMotor controlled by this motor pair.
+   * @return The first motor controlled by this pair.
+   */
   public DcMotor getMotor1() {
     return motor1;
   }
 
+  /**
+   * Gets the second motor controlled by this motor pair.
+   * @return The second motor controlled by this pair.
+   */
   public DcMotor getMotor2() {
     return motor2;
   }
 
+  /**
+   * Sets the first motor controlled by this motor pair.
+   * @param motor1 The motor to be controlled by this pair.
+   * @throws Exception Thrown if no motors are remaining in this pair.
+   */
   public void setMotor1(DcMotor motor1)throws Exception {
     this.motor1 = motor1;
     if (this.motor1 == null && motor2 != null) {
@@ -49,6 +72,11 @@ public class DcMotorPair {
 
   }
 
+  /**
+   * Sets the second motor controlled by this motor pair.
+   * @param motor2 The motor to be controlled by this pair.
+   * @throws Exception Thrown if no motors are remaining in this pair.
+   */
   public void setMotor2(DcMotor motor2) throws Exception {
     this.motor2 = motor2;
     if (motor1 != null && this.motor2 == null) {
@@ -60,11 +88,19 @@ public class DcMotorPair {
     }
   }
 
+  /**
+   * Returns true if this pair only controls one motor.
+   * @return true if this pair only controls one motor.
+   */
   public boolean isSingleMotor() {
     return isSingleMotor;
   }
 
 
+  /**
+   * Sets the power of all motors in this pair.
+   * @param power The power to set to each motor.
+   */
   public void setPower(double power) {
     if(motor1 != null)
       motor1.setPower(power);
@@ -72,6 +108,11 @@ public class DcMotorPair {
       motor2.setPower(power);
   }
 
+  /**
+   * Sets the direction of all motors in the pair.
+   * Calls <code>DcMotor.setDirection(Direction)</code> on each motor.
+   * @param direction The <code>DcMotor.Direction</code> to set the direction of the motors to.
+   */
   public void setDirection(DcMotor.Direction direction) {
     if(motor1 != null)
       motor1.setDirection(direction);
@@ -79,6 +120,10 @@ public class DcMotorPair {
       motor2.setDirection(direction);
   }
 
+  /**
+   * Sets the current run mode of all motors in the pair.
+   * @param runMode The run mode to set for each motor.
+   */
   public void setMode(DcMotor.RunMode runMode) {
     if(motor1 != null)
       motor1.setMode(runMode);
@@ -87,6 +132,11 @@ public class DcMotorPair {
     }
   }
 
+  /**
+   * Sets the target position of all motors in the pair.
+   * @param targetPosition The target position (in encoder ticks) to set each
+   *                       motor's target position to.
+   */
   public void setTargetPosition(int targetPosition) {
     if(motor1 != null)
       motor1.setTargetPosition(targetPosition);
@@ -95,6 +145,12 @@ public class DcMotorPair {
     }
   }
 
+  /**
+   * Gets the current position of the first motor if there are two motors,
+   * otherwise, gets the current position of the single motor.
+   * @return The position of the motor(s) in encoder ticks.
+   * @throws Exception Thrown when no motors were found in the current pair.
+   */
   public int getCurrentPosition() throws Exception {
     if(motor1 != null && motor2 != null) {
       return motor1.getCurrentPosition();
